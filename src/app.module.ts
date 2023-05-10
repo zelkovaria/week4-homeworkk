7d0599f3cd0a5a7d7de718a283ad3fa858030287
zelkovaria <110520124+zelkovaria@users.noreply.github.com>
2023-05-10 23:07:55 +0900

    5week homework

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, EmailModule],
   controllers: [],
   providers: [],
 })
